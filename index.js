ced40e669968397262e93c58e46077fb06fee44d
Bernardo <gurmock@gmail.com>
2022-12-08 12:48:20 +0100

    userschema

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const { urlencoded } = require('express')
-require('dotenv').config()
-const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { urlencoded } = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI,() =>{
-  console.log('Connected DB successfully');
+mongoose.connect(process.env.MONGO_URI, () => {
+  console.log("Connected DB successfully");
 });
-app.use(cors())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+    },
+  },
+  { versionKey: false }
+);
+
+const User = mongoose.model("user", userSchema);
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 //POST  TO /api/users username
-app.post('/api/users',(req, res ) =>{
+app.post("/api/users", async (req, res) => {
   const username = req.body.username;
-})
 
+  const foundUser = User.findOne({ username });
 
+  if(){
+    
+  }
 
+  const user = await User.create({
+    username,
+  });
 
+  res.json(user);
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
