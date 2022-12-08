9dac4af307301912e37a6857c5ac13a17c94979b
Bernardo <gurmock@gmail.com>
2022-12-08 12:36:15 +0100

    db connect

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { urlencoded } = require('express')
 require('dotenv').config()
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO_URI,() =>{
+  console.log('Connected DB successfully');
+});
 app.use(cors())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+//POST  TO /api/users username
+app.post('/api/users',(req, res ) =>{
+  const username = req.body.username;
+})
 
 
 
